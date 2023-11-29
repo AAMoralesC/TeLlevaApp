e894005b2308e53de4862fc3037296575875410d
Andres Morales <andr.moralesc@duocuc.cl>
2023-11-29 18:33:54 -0300

    arreglos

--- src/app/confirmar-viaje-p/confirmar-viaje-p.page.ts
+++ src/app/confirmar-viaje-p/confirmar-viaje-p.page.ts
@@ -1,16 +1,21 @@
-import { Component,ElementRef,NgZone,ViewChild, OnInit } from '@angular/core';
-import { Platform } from '@ionic/angular';
+import { Component} from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { Storage } from '@ionic/storage-angular';
 
-declare var google: any;
+
 
 export interface Pasajero{
   nombrePas:string,
   celular:string,
   carrera:string
 }
-
+export interface Viaje{
+  id: number,
+  hora: string,
+  asientos: number,
+  destino: string,
+  precio: number
+}
 
 @Component({
   selector: 'app-confirmar-viaje-p',
@@ -18,6 +23,8 @@ export interface Pasajero{
   styleUrls: ['./confirmar-viaje-p.page.scss'],
 })
 export class ConfirmarViajePPage {
+  viajeSeleccionado: any;
+  viajes: any[] = [];
 
   agregar:Pasajero={ 
     nombrePas:"",
@@ -25,12 +32,14 @@ export class ConfirmarViajePPage {
     carrera:""
    }
 
-  constructor(private platform:Platform, private zone:NgZone, private alertController: AlertController, private storage: Storage) { }
+  constructor(private alertController: AlertController, private storage: Storage) { 
+
+  }
 
   async ngOnInit(){
-  
     await this.storage.create(); 
   }
+
 
   async guardarPas(){
     let pasajeros = await this.storage.get("pasajeros") || []; 
@@ -39,79 +48,9 @@ export class ConfirmarViajePPage {
     this.storage.set("pasajeros", pasajeros)
     console.log(pasajeros)
   }
-
-
-  
-  @ViewChild('map') mapElement: ElementRef | undefined;
-  public map: any;
-  public start: any = "Duoc UC: Sede Melipilla - Serrano, Melipilla, Chile";
-  public end: any = "Melipilla";
-  public latitude: any;
-  public longitude: any;
-  public directionsService: any;
-  public directionsDisplay: any;
-  public autocompleteItems: any
-
-
-
-  ionViewDidEnter() {
-    this.platform.ready().then(() => {
-      this.initMap()
-    })
+  async ionViewDidEnter() {
+    this.viajes = await this.storage.get("viajes") || []
   }
-
-  initMap() {
-    this.directionsService = new google.maps.DirectionsService;
-    this.directionsDisplay = new google.maps.DirectionsRenderer;
-    let latLng = new google.maps.LatLng(this.latitude, this.longitude);
-    let mapOptions = {
-      center: latLng,
-      zoom: 5,
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      disableDefaultUI: true
-      
-    };
-    this.map = new google.maps.Map(this.mapElement!.nativeElement, mapOptions);
-    this.directionsDisplay.setMap(this.map);
-    this.calculateAndDisplayRoute();
-  }
-
-  calculateAndDisplayRoute() {
-    this.directionsService.route({
-      origin: this.start,
-      destination: this.end,
-      travelMode: 'DRIVING'
-    }, (response: any, status: string) => {
-      if (status === 'OK') {
-        this.directionsDisplay.setDirections(response);
-      } else {
-        window.alert('Directions request failed due to ' + status);
-      }
-    });
-  }
-
-  updateSearchResults() {
-    let GoogleAutocomplete = new google.maps.places.AutocompleteService();
-    if (this.end == '') {
-      this.autocompleteItems = [];
-      return;
-    }
-    GoogleAutocomplete!.getPlacePredictions({ input: this.end },
-      (predictions: any, status: any) => {
-        this.autocompleteItems = [];
-        this.zone.run(() => {
-          predictions.forEach((prediction: any) => {
-            this.autocompleteItems!.push(prediction);
-          });
-        });
-      });
-  }
-  selectSearchResult(item: any) {
-    this.end = item.description
-    this.autocompleteItems = []
-    this.initMap()
-  }
-
   async presentAlert(){
     const alert=await this.alertController.create({
       header: 'Solicitando Viaje...',
@@ -119,5 +58,12 @@ export class ConfirmarViajePPage {
       buttons: ['Ok'],
     });
     await alert.present()
+  }
+  async ionViewWillEnter() {
+    try {
+      this.viajeSeleccionado = await this.storage.get('viajeSeleccionado');
+    } catch (error) {
+      console.error('Error al recuperar el viaje seleccionado', error);
+    }
   }
 }

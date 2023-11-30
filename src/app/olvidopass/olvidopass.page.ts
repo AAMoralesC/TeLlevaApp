7e793e4c8808306b73cc89f9980390feda806dd5
Andres Morales <andr.moralesc@duocuc.cl>
2023-11-29 23:26:54 -0300

    casi fin

--- src/app/olvidopass/olvidopass.page.ts
+++ src/app/olvidopass/olvidopass.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-olvidopass',
   templateUrl: './olvidopass.page.html',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OlvidopassPage implements OnInit {
 
-  constructor() { }
+  constructor(private alertController: AlertController) { }
 
   ngOnInit() {
   }
-
+  async presentAlert(){
+    const alert=await this.alertController.create({
+      header: 'Revisa tu email',
+      message:'Hemos enviado un correo con tu nueva contraseña',
+      buttons: ['Ok'],
+    });
+    await alert.present()
+  }
 }
